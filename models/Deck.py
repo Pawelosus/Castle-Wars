7ad613b1ccf6966e388cc3c269ba62c51bcad16e
Pawelosus <pawelosuspl@gmail.com>
2024-11-07 03:54:07 +0100

    Updated Deck.py - Deck objects can now be created using custom .json files

--- models/Deck.py
+++ models/Deck.py
@@ -1,32 +1,48 @@
 import json
 from models.Card import Card
 from random import shuffle
+from pathlib import Path
 from typing import Union
 
 class Deck:
-    def __init__(self) -> None:
-        self.cards = self.init_deck()
+    def __init__(self, deck_file: Union[str, Path] = 'default_deck.json') -> None:
+        self.cards = self.init_deck(deck_file)
 
-    def init_deck(self) -> list:
+    def init_deck(self, deck_file: Union[str, Path]) -> list:
         """Init deck with default deck."""
-        with open('resources/decks/default_deck.json') as f:
-            deck_dict = json.load(f)
+        deck_dir = Path(__file__).parent.parent / 'resources' / 'decks'
+        deck_path = deck_dir / deck_file
         
-        deck = list()
-        with open('resources/cards.json') as f:
-            cards_data = json.load(f)
-            for card_id, count in deck_dict.items():
-                material_type, index = card_id.split(':')
-                card_info = cards_data[material_type][int(index)]
-                for _ in range(count):
-                    card = Card(
-                        card_id,
-                        material_type,
-                        card_info['cost'],
-                        card_info['name'],
-                        card_info['effect']
-                    )
-                    deck.append(card)
+        # Load the deck configuration
+        try:
+            with open(deck_path) as f:
+                deck_dict = json.load(f)
+        except FileNotFoundError:
+            raise ValueError(f'Deck file "{deck_path}" not found.')
+        
+        deck = []
+
+        # Load base card info
+        cards_path = Path(__file__).parent.parent / 'resources' / 'cards.json'
+        try:
+            with open(cards_path) as f:
+                cards_data = json.load(f)
+        except FileNotFoundError:
+            raise ValueError(f'Card data file "{cards_path}" not found.')
+
+        # Initialize cards based on deck configuration
+        for card_id, count in deck_dict.items():
+            material_type, index = card_id.split(':')
+            card_info = cards_data[material_type][int(index)]
+            for _ in range(count):
+                card = Card(
+                    card_id,
+                    material_type,
+                    card_info['cost'],
+                    card_info['name'],
+                    card_info['effect']
+                )
+                deck.append(card)
 
         shuffle(deck)  # Using the built-in random.shuffle function
         return deck
